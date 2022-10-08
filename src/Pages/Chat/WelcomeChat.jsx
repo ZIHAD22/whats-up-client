596b3ac1f03bf961ec357b2e18576a26f85ecb54
ZIHAD22 <mdzihad2244@gmail.com>
2022-10-08 21:39:27 +0600

    welcome page add

--- src/Pages/Chat/WelcomeChat.jsx
+++ src/Pages/Chat/WelcomeChat.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Spinner from '../../Components/Spinner';
 
 const WelcomeChat = () => {
     const [user , loading] = useSelector(state => [state.authUser.user , state.authUser.authUserLoading])
     
+    if(loading) {
+        return <Spinner/>
+    }
+
     return (
         <div className='text-center mt-8'>
             <h1 className='text-3xl'>Welcome</h1>
